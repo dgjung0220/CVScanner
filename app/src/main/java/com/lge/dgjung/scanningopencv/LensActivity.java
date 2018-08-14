ace0ddd09161649ad62610534b004975e5825c1f
dg.jung <dgjung0220@gmail.com>
2018-08-14 13:43:14 +0900

    demo final

--- app/src/main/java/com/lge/dgjung/scanningopencv/LensActivity.java
+++ app/src/main/java/com/lge/dgjung/scanningopencv/LensActivity.java
@@ -1,5 +1,6 @@
 package com.lge.dgjung.scanningopencv;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -25,10 +26,10 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.core.TermCriteria;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.utils.Converters;
 import org.opencv.core.Point;
@@ -48,7 +49,7 @@ public class LensActivity extends AppCompatActivity {
     private static final String FILE_LOCATION = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString() + File.separator + "Camera";
     private static final String photoPath_debugging = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString() + File.separator + "debugging_drawing"+ ".png";
 
-    private ImageView ivImage;
+    private ImageView ivImage, ivImage1, ivImage2;
 
     private String errorMsg;
     private Uri fileUri;
@@ -57,6 +58,8 @@ public class LensActivity extends AppCompatActivity {
     private Mat srcOrig;
     static int scaleFactor;
 
+    ArrayList<Point> cornersCopy = new ArrayList<>();
+    ArrayList<Bitmap> bitmaplist = new ArrayList<>();
     Bitmap bitmap;
 
     @Override
@@ -99,6 +102,8 @@ public class LensActivity extends AppCompatActivity {
         }
 
         ivImage = (ImageView) findViewById(R.id.ivImage);
+        ivImage1 = (ImageView) findViewById(R.id.ivImage1);
+        ivImage2 = (ImageView) findViewById(R.id.ivImage2);
 
         Button bClickImage, bLoadImage;
 
@@ -116,12 +121,10 @@ public class LensActivity extends AppCompatActivity {
                 imagesFolder.mkdirs();
 
                 File image = new File(imagesFolder, "image_10.jpg");
-                // fileUri = Uri.fromFile(image);
                 fileUri = FileProvider.getUriForFile(getApplicationContext(), "com.lge.dgjung.fileprovider", image);
                 Log.d("LensJDG", "File URI = " + fileUri.toString());
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
 
-                // start the image capture Intent
                 startActivityForResult(intent, CLICK_PHOTO);
             }
         });
@@ -140,18 +143,14 @@ public class LensActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int
-            resultCode, Intent imageReturnedIntent) {
-        super.onActivityResult(requestCode, resultCode,
-                imageReturnedIntent);
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
         Log.d(TAG, requestCode + " " + CLICK_PHOTO + resultCode + " " + RESULT_OK);
 
         switch(requestCode) {
             case CLICK_PHOTO:
                 if(resultCode == RESULT_OK){
                     try {
-                        Log.d("LensJDG", fileUri.toString());
-
                         final InputStream imageStream = getContentResolver().openInputStream(fileUri);
                         final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
                         srcOrig = new Mat(selectedImage.getHeight(), selectedImage.getWidth(), CvType.CV_8UC4);
@@ -159,9 +158,8 @@ public class LensActivity extends AppCompatActivity {
                         src = new Mat();
                         Utils.bitmapToMat(selectedImage, srcOrig);
                         scaleFactor = calcScaleFactor(srcOrig.rows(), srcOrig.cols());
-                        Imgproc.resize(srcOrig, src, new Size(srcOrig.rows()/scaleFactor,srcOrig.cols()/scaleFactor));
-                        //Imgproc.resize(srcOrig, src, new Size(srcOrig.rows(), 500));
 
+                        Imgproc.resize(srcOrig, src, new Size(srcOrig.rows()/scaleFactor,srcOrig.cols()/scaleFactor));
                         getPage();
 
                     } catch (FileNotFoundException e) {
@@ -175,7 +173,7 @@ public class LensActivity extends AppCompatActivity {
                         InputStream stream = getContentResolver().openInputStream(imageReturnedIntent.getData());
                         final Bitmap selectedImage = BitmapFactory.decodeStream(stream);
                         stream.close();
-                        ivImage.setImageBitmap(selectedImage);
+
                         srcOrig = new Mat(selectedImage.getHeight(), selectedImage.getWidth(), CvType.CV_8UC4);
                         Imgproc.cvtColor(srcOrig, srcOrig, Imgproc.COLOR_BGR2RGB);
                         Utils.bitmapToMat(selectedImage, srcOrig);
@@ -205,6 +203,7 @@ public class LensActivity extends AppCompatActivity {
             idealCol = 240;
             idealRow = 320;
         }
+
         int val = Math.min(rows / idealRow, cols / idealCol);
         if(val<=0){
             return 1;
@@ -219,12 +218,11 @@ public class LensActivity extends AppCompatActivity {
     }
 
     static Point findIntersection(double[] line1, double[] line2) {
-        double start_x1 = line1[0], start_y1 = line1[1],
-                end_x1 = line1[2], end_y1 = line1[3], start_x2 =
-                line2[0], start_y2 = line2[1], end_x2 = line2[2],
-                end_y2 = line2[3];
-        double denominator = ((start_x1 - end_x1) * (start_y2 -
-                end_y2)) - ((start_y1 - end_y1) * (start_x2 - end_x2));
+        double start_x1 = line1[0], start_y1 = line1[1], end_x1 = line1[2], end_y1 = line1[3],
+                start_x2 = line2[0], start_y2 = line2[1], end_x2 = line2[2], end_y2 = line2[3];
+
+        double denominator = ((start_x1 - end_x1) * (start_y2 - end_y2)) - ((start_y1 - end_y1) * (start_x2 - end_x2));
+
         if (denominator!=0) {
             Point pt = new Point();
             pt.x = (int) (((start_x1 * end_y1 - start_y1 * end_x1) * (start_x2 - end_x2) - (start_x1 - end_x1) * (start_x2 * end_y2 - start_y2 * end_x2)) / denominator);
@@ -262,16 +260,14 @@ public class LensActivity extends AppCompatActivity {
             else
                 bottom.add(corners.get(i));
         }
+
         corners.clear();
+
         if (top.size() == 2 && bottom.size() == 2){
-            Point top_left = top.get(0).x > top.get(1).x ?
-                    top.get(1) : top.get(0);
-            Point top_right = top.get(0).x > top.get(1).x ?
-                    top.get(0) : top.get(1);
-            Point bottom_left = bottom.get(0).x > bottom.get(1).x
-                    ? bottom.get(1) : bottom.get(0);
-            Point bottom_right = bottom.get(0).x > bottom.get(1).x
-                    ? bottom.get(0) : bottom.get(1);
+            Point top_left = top.get(0).x > top.get(1).x ? top.get(1) : top.get(0);
+            Point top_right = top.get(0).x > top.get(1).x ? top.get(0) : top.get(1);
+            Point bottom_left = bottom.get(0).x > bottom.get(1).x ? bottom.get(1) : bottom.get(0);
+            Point bottom_right = bottom.get(0).x > bottom.get(1).x ? bottom.get(0) : bottom.get(1);
             top_left.x *= scaleFactor;
             top_left.y *= scaleFactor;
             top_right.x *= scaleFactor;
@@ -289,14 +285,20 @@ public class LensActivity extends AppCompatActivity {
 
     private void getPage() {
 
-        new AsyncTask<Void, Void, Bitmap>() {
+        new AsyncTask<Void, Void, ArrayList<Bitmap>>() {
+            ProgressDialog dialog;
+
             @Override
             protected void onPreExecute() {
                 super.onPreExecute();
+                dialog = ProgressDialog.show(LensActivity.this, "Grabbing page", "Please Wait");
             }
 
             @Override
-            protected Bitmap doInBackground(Void... voids) {
+            protected ArrayList<Bitmap> doInBackground(Void... voids) {
+                Mat srcRes = new Mat( src.size(), src.type() );
+                Mat srcGray = new Mat();
+
                 Mat samples = new Mat(src.rows() * src.cols(), 3, CvType.CV_32F);
                 for( int y = 0; y < src.rows(); y++ ) {
                     for( int x = 0; x < src.cols(); x++ ) {
@@ -316,9 +318,6 @@ public class LensActivity extends AppCompatActivity {
                 double dstCenter1 = calcWhiteDist(centers.get(1, 0)[0], centers.get(1, 1)[0], centers.get(1, 2)[0]);
                 int paperCluster = (dstCenter0 < dstCenter1)?0:1;
 
-                Mat srcRes = new Mat( src.size(), src.type() );
-                Mat srcGray = new Mat();
-
                 for( int y = 0; y < src.rows(); y++ ) {
                     for( int x = 0; x < src.cols(); x++ )
                     {
@@ -332,126 +331,93 @@ public class LensActivity extends AppCompatActivity {
                 }
 
                 Imgproc.cvtColor(src, srcGray, Imgproc.COLOR_BGR2GRAY);
-                Imgproc.GaussianBlur(srcGray, srcGray, new Size(5, 5), 0);
-                Imgproc.Canny(srcGray, srcGray, 75, 200);
-
+                Imgproc.Canny(srcGray, srcGray, 50, 150);
                 List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
                 Mat hierarchy = new Mat();
-                Imgproc.findContours(srcGray, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-
-                int index = 0;
-                double maxim = Imgproc.contourArea(contours.get(0));
-
-                for (int contourIdx = 1; contourIdx < contours.size(); contourIdx++) {
-                    double temp;
-                    temp=Imgproc.contourArea(contours.get(contourIdx));
-                    if(maxim<temp)
-                    {
-                        maxim=temp;
-                        index=contourIdx;
-                    }
-                }
 
                 Mat drawing = Mat.zeros(srcRes.size(), CvType.CV_8UC1);
+                Imgproc.findContours(srcGray, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
 
-                //Imgproc.drawContours(src, contours, index, new Scalar(0,255,0), 10);
-                Imgproc.drawContours(drawing, contours, index, new Scalar(255), 1);
-/*
-                if(!Imgcodecs.imwrite(photoPath_debugging, drawing)) {
-                    Log.e(TAG, "Failed to save photo to " + photoPath_debugging);
-                    onTakePhotoFailed();
+                MatOfPoint approxContour = new MatOfPoint();
+                MatOfPoint2f thisContour2f = new MatOfPoint2f();
+                MatOfPoint2f approxContour2f = new MatOfPoint2f();
+                List<Point> foundCnt = new ArrayList<>();
+
+                int index = 0;
+                for (MatOfPoint c : contours) {
+                    c.convertTo(thisContour2f, CvType.CV_32FC2);
+
+                    double peri = Imgproc.arcLength(thisContour2f, true);
+                    Imgproc.approxPolyDP(thisContour2f, approxContour2f, 0.02 * peri, true);
+                    approxContour2f.convertTo(approxContour, CvType.CV_32S);
+
+                    if (approxContour.size().height == 4) {
+                        for (Point p : approxContour.toArray()) {
+                           foundCnt.add(p);
+                        }
+                        break;
+                    }
+                    index++;
                 }
-*/
-                Bitmap bitmap = Bitmap.createBitmap(src.cols(), src.rows(), Bitmap.Config.ARGB_8888);
+
+                Imgproc.drawContours(src, contours, index, new Scalar(255), 10);
+                Imgproc.drawContours(drawing, contours, index, new Scalar(255), 10);
+
+                bitmap = Bitmap.createBitmap(drawing.cols(), drawing.rows(), Bitmap.Config.ARGB_8888);
+                Utils.matToBitmap(drawing, bitmap);
+                bitmaplist.add(bitmap);
+
+                bitmap = Bitmap.createBitmap(src.cols(), src.rows(), Bitmap.Config.ARGB_8888);
                 Utils.matToBitmap(src, bitmap);
+                bitmaplist.add(bitmap);
 
-                Mat lines = new Mat();
-                Imgproc.HoughLines(drawing, lines, 1, Math.PI/180, 70);
-
-                Log.d(TAG, "lines : " + lines.cols());
-                List<Point> corners = new ArrayList<>();
-                for (int i = 0; i < lines.cols(); i++) {
-                    for (int j = i + 1; j < lines.cols(); j++) {
-                        double[] line1 = lines.get(0, i);
-                        double[] line2 = lines.get(0, j);
-
-                        Point pt = findIntersection(line1, line2);
-                        Log.d(TAG, pt.x + " " + pt.y);
-
-                        if (pt.x >= 0 && pt.y >= 0 && pt.x <= drawing.cols() && pt.y <= drawing.rows()) {
-                            if (!exists(corners, pt)) {
-                                corners.add(pt);
-                            }
-                        }
-                    }
+                ArrayList<Point> corners = new ArrayList<>();
+                for (Point p : foundCnt) {
+                    Log.d(TAG, p.x + "," + p.y);
+                    corners.add(new Point(p.x,p.y));
                 }
 
-                /*List<Point> corners = new ArrayList<>();
-                for (int i = 0; i < lines.cols(); i++) {
-                    for (int j = i + 1; j <= lines.cols(); j++) {
-                        double[] line1 = lines.get(0, i);
-                        double[] line2 = lines.get(0, j);
+                sortCorners(corners);
 
-                        Point pt = findIntersection(line1, line2);
-                        Log.d(TAG, pt.x + " " + pt.y);
+                double top = Math.sqrt(Math.pow(corners.get(0).x - corners.get(1).x, 2) + Math.pow(corners.get(0).y - corners.get(1).y, 2));
+                double right = Math.sqrt(Math.pow(corners.get(1).x - corners.get(2).x, 2) + Math.pow(corners.get(1).y - corners.get(2).y, 2));
+                double bottom = Math.sqrt(Math.pow(corners.get(2).x - corners.get(3).x, 2) + Math.pow(corners.get(2).y - corners.get(3).y, 2));
+                double left = Math.sqrt(Math.pow(corners.get(3).x - corners.get(1).x, 2) + Math.pow(corners.get(3).y - corners.get(1).y, 2));
 
-                        if (pt.x >= 0 && pt.y >= 0 && pt.x <=
-                                drawing.cols() && pt.y <= drawing.rows()) {
-                            if (!exists(corners, pt)) {
-                                corners.add(pt);
-                            }
-                        }
-                    }
-                }
+                Mat quad = Mat.zeros(new Size(Math.max(top, bottom), Math.max(left, right)), CvType.CV_8UC3);
 
-                Log.d(TAG, "Corner size : " + corners.size());
-                if(corners.size() != 4){
-                    errorMsg = "Cannot detect perfect corners";
-                    return null;
-                }
-
-                double top = Math.sqrt(Math.pow(corners.get(0).x -
-                        corners.get(1).x, 2) + Math.pow(corners.get(0).y -
-                        corners.get(1).y, 2));
-                double right = Math.sqrt(Math.pow(corners.get(1).x -
-                        corners.get(2).x, 2) + Math.pow(corners.get(1).y -
-                        corners.get(2).y, 2));
-                double bottom = Math.sqrt(Math.pow(corners.get(2).x -
-                        corners.get(3).x, 2) + Math.pow(corners.get(2).y -
-                        corners.get(3).y, 2));
-                double left = Math.sqrt(Math.pow(corners.get(3).x -
-                        corners.get(1).x, 2) + Math.pow(corners.get(3).y -
-                        corners.get(1).y, 2));
-                Mat quad = Mat.zeros(new Size(Math.max(top, bottom),
-                        Math.max(left, right)), CvType.CV_8UC3);
-
-                List<Point> result_pts = new ArrayList<>();
+                ArrayList<Point> result_pts = new ArrayList<>();
                 result_pts.add(new Point(0, 0));
-                result_pts.add(new Point(quad.cols(), 0));
-                result_pts.add(new Point(quad.cols(), quad.rows()));
-                result_pts.add(new Point(0, quad.rows()));
+                result_pts.add(new Point(quad.cols()-1, 0));
+                result_pts.add(new Point(quad.cols()-1, quad.rows()-1));
+                result_pts.add(new Point(0, quad.rows()-1));
 
                 Mat cornerPts = Converters.vector_Point2f_to_Mat(corners);
                 Mat resultPts = Converters.vector_Point2f_to_Mat(result_pts);
+
                 Mat transformation = Imgproc.getPerspectiveTransform(cornerPts, resultPts);
-                Imgproc.warpPerspective(srcOrig, quad, transformation, quad.size());
+                Imgproc.warpPerspective(src, quad, transformation, quad.size());
 
                 Imgproc.cvtColor(quad, quad, Imgproc.COLOR_BGR2RGBA);
-                Bitmap bitmap = Bitmap.createBitmap(quad.cols(), quad.rows(), Bitmap.Config.ARGB_8888);
+
+                bitmap = Bitmap.createBitmap(quad.cols(), quad.rows(), Bitmap.Config.ARGB_8888);
                 Utils.matToBitmap(quad, bitmap);
-*/
-                return bitmap;
+                bitmaplist.add(bitmap);
+
+                return bitmaplist;
             }
 
             @Override
-            protected void onPostExecute(Bitmap bitmap) {
+            protected void onPostExecute(ArrayList<Bitmap> bitmap) {
                 super.onPostExecute(bitmap);
+                dialog.dismiss();
 
                 if(bitmap!=null) {
-                    ivImage.setImageBitmap(bitmap);
+                    ivImage.setImageBitmap(bitmap.get(0));
+                    ivImage1.setImageBitmap(bitmap.get(1));
+                    ivImage2.setImageBitmap(bitmap.get(2));
                 } else if (errorMsg != null){
-                    Toast.makeText(getApplicationContext(),
-                            errorMsg, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_SHORT).show();
                 }
             }
         }.execute();
